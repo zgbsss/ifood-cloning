a85f439376b0722fea5e720041d668f21dc90331
Sam "Felepopolo Jaré" Kuhl <99222712+felipejare@users.noreply.github.com>
2023-06-07 16:51:38 -0300

    Criação do Projeto Ifood Cloning

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { Fragment } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Fragment>
+      <StatusBar style="auto" />
+      <Routes />
+    </Fragment>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
