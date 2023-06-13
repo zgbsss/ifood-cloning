b3b9240c121ab3c79b3b1b12e8e46ff8d7d467cc
felipejare <felipeakf@gmail.com>
2023-06-13 15:11:08 -0300

    Adição de conteudos na screen Perfil

--- src/screens/Perfil/index.js
+++ src/screens/Perfil/index.js
@@ -1,12 +1,121 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet,TouchableOpacity, ScrollView } from 'react-native';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 
 export default function Perfil() {
     return( 
-        <View style={styles.container} > 
-            <Text>Perfil</Text>
+        <ScrollView style={styles.container} >
+            <ScrollView>
+                <TouchableOpacity style={styles.options} onPress={() => {}} >
+                    <MaterialCommunityIcons name="bell-outline" size={35} color="#333"/>
+                    <ScrollView style={styles.info} >
+                        <Text style={styles.title}> Notificações </Text>
+                        <Text style={styles.description}> Minha Central de Notificações </Text>
+                    </ScrollView>
+                    <MaterialCommunityIcons name="keyboard-arrow-right" color="#999" size={20} />
+                </TouchableOpacity>
+                <TouchableOpacity 
+                style={styles.options}
+                onPress={() => navigation.navigate ('Pagamentos')}>
+                    <MaterialCommunityIcons name="credit-card" size={35} color="#333"/>
+                    <ScrollView style={styles.info}>
+                        <Text style={styles.title}>Pagamentos</Text>
+                        <Text style={styles.description}>Meus Saldos e cartões</Text>
+                    </ScrollView>
+                    <MaterialIcons name="keyboard-arrow-right" color='#999' size={20}/>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <MaterialCommunityIcons 
+                        name="ticket-outline"
+                        size={35}
+                        color="333"
+                    />
+                    <ScrollView style={styles.info}>
+                        <Text style={styles.title}>Cupons</Text>
+                        <Text style={styles.description}>Meus Cupons de Desconto</Text>
+                    </ScrollView>
+                    <MaterialIcons name="keyboard-arrow-right" color="#999" size={20}/>
+                </TouchableOpacity>
+                
+                <TouchableOpacity style={styles.option} onPress={() => {}}>
+                    <MaterialCommunityIcons name="heart-outline" size={35} color="#333" />
+                        <ScrollView style={styles.info}>
+                            <Text style={styles.title}>Favoritos</Text>
+                            <Text style={styles.description}>Meus locais favoritos</Text>
+                        </ScrollView>
+                    <MaterialIcons name="keyboard-arrow-right" color="#999" size={20} />
+                </TouchableOpacity>
 
-        </View>
+                <TouchableOpacity style={styles.option} onPress={() => {}}>
+                <MaterialCommunityIcons name="credit-card" size={35} color="#333" />
+                <ScrollView style={styles.info}>
+                    <Text style={styles.title}>Fidelidade</Text>
+                    <Text style={styles.description}>Minhas fidelidades</Text>
+                </ScrollView>
+                <MaterialIcons name="keyboard-arrow-right" color="#999" size={20} />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.option} onPress={() => {}}>
+                <MaterialCommunityIcons name="map-marker" size={35} color="#333" />
+                <ScrollView style={styles.info}>
+                    <Text style={styles.title}>Endereços</Text>
+                    <Text style={styles.description}>Meus endereços de entrega</Text>
+                </ScrollView>
+                <MaterialIcons name="keyboard-arrow-right" color="#999" size={20} />
+                </TouchableOpacity>
+
+                <View style={styles.menuAdicional}>
+                    <TouchableOpacity style={styles.opcoesAdicionais}>
+                        <View style={styles.wrapper}>
+                            <MaterialCommunityIcons name="lifebuoy" size={25} color="#CDC"/>
+                            <Text style={styles.optionsName}>Ajuda</Text>
+                        </View>
+                        <MaterialIcons name="keyboard-arrow-right" color="#999" size={20} />
+                    </TouchableOpacity>
+
+
+                    <TouchableOpacity style={styles.opcoesAdicionais}>
+                    <View style={styles.wrapper}>
+                        <MaterialIcons name="settings" size={25} color="#CDC" />
+                        <Text style={styles.optionName}>Configurações</Text>
+                    </View>
+                    <MaterialIcons name="keyboard-arrow-right" color="#999" size={20} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.opcoesAdicionais}>
+                    <View style={styles.wrapper}>
+                        <MaterialIcons name="security" size={25} color="#CDC" />
+                        <Text style={styles.optionName}>Segurança</Text>
+                    </View>
+                    <MaterialIcons name="keyboard-arrow-right" color="#999" size={20} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.opcoesAdicionais}>
+                    <View style={styles.wrapper}>
+                        <MaterialIcons name="store-mall-directory" size={25} color="#CDC" />
+                        <Text style={styles.optionName}>Sugerir Restaurantes</Text>
+                    </View>
+                    <MaterialIcons name="keyboard-arrow-right" color="#999" size={20} />
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.opcoesAdicionais}>
+                    <View style={styles.wrapper}>
+                        <MaterialCommunityIcons name="rocket" size={25} color="#CDC" />
+                        <Text style={styles.optionName}>Seja parceiro!</Text>
+                    </View>
+                    <MaterialIcons name="keyboard-arrow-right" color="#999" size={20} />
+                    </TouchableOpacity>
+
+
+
+
+                </View>
+            </ScrollView>
+
+        </ScrollView>
+
+
+
 
 
     );
@@ -14,9 +123,56 @@ export default function Perfil() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex:1,
         backgroundColor: '#fff',
+    },
+    option: {
+        flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'center',
-    }
+        justifyContent: 'space-around',
+        marginTop: 5,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        border: 1,
+
+    },
+    info: {
+        marginLeft: 20,
+    },
+    title: {
+        color: '#333',
+        fontSize: 18,
+    },
+    description: {
+        fontSize: 16,
+        color: '#999',
+    },
+    menuAdicional: {
+        marginTop: 30,
+    },
+    opcoesAdicionais: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginTop: 5,
+        marginBottom: 5,
+        marginLeft: 0,
+        marginRight: 0,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        border: 1,
+      },
+      wrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+      },
+      optionName: {
+        marginLeft: 25,
+        color: '#ccc',
+        fontSize: 15,
+      },
 });
