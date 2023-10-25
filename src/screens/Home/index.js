85940512ecb3182f6d8230b5c1fdba32ea78ebd6
zgbsss <gabrielasaraiva.04@gmail.com>
2023-10-25 16:51:00 -0300

    Mudanças na Home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ScrollView, View, StyleSheet } from "react-native";
+import { ScrollView, View, StyleSheet, Text } from "react-native";
 
 import CupomDesconto from "../../components/Home/CupomDesconto";
 import Ofertas from "../../components/Home/Ofertas";
@@ -17,11 +17,14 @@ export default function Home({ navigation }) {
       <Ofertas navigation={navigation} />
       <ScrollView >
         <CupomDesconto />
-          <Bloco key={1} />
-          <Bloco key={2} />
-          <Bloco key={3} />
-          <Bloco key={4} />
-          <Bloco key={5} />
+        <Text style={styles.titulo}>Hamburgueres</Text>
+        <View style={styles.bloco}>
+            <Bloco key={1} />
+            <Bloco key={2} />
+            <Bloco key={3} />
+            <Bloco key={4} />
+            <Bloco key={5} />
+        </View>
       </ScrollView>
     </View>
   );
@@ -31,5 +34,15 @@ const styles = StyleSheet.create({
   container: {
     height: 1000,
     backgroundColor: "#fff",
+  },
+  titulo: {
+    fontSize: 23,
+    fontWeight: "bold",
+    marginTop: 15,
+    marginRight: 10,
+    marginLeft: 20,
+  },
+  bloco: {
+    display: "flex",
   },
 });
